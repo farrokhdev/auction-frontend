b70e59da084a794d17e33d5c61f82e5be24fc2ff
samira rezaei <samirarezai1996@gmail.com>
2021-06-30 16:24:15 +0430

    complete connect api profile

--- src/Views/PanleProfile/EditPhoneNumberPanelProfile.jsx
+++ src/Views/PanleProfile/EditPhoneNumberPanelProfile.jsx
@@ -1,58 +1,161 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {Button, Form, Input, message, Spin} from "antd";
+import axios from "../../utils/request";
+import {BASE_URL} from "../../utils";
+import {ACCOUNT_APPROVE, EDIT_PROFILE} from "../../utils/constant";
 
-function EditPhoneNumberPanelProfile() {
+function EditPhoneNumberPanelProfile(props) {
+    const [form] = Form.useForm();
+    const [loading, setLoading] = useState(false)
+    const [showNumber, setShowNumber] = useState(true)
+    const [number, setNumber] = useState("")
+    const {data,getProfile} = props;
+    useEffect(()=>{
+        if(data.mobile){
+            setNumber(data.mobile)
+            form.setFieldsValue({mobile:data.mobile})
+        }
+
+    },[data])
+    const onFinish = (values) => {
+        if (values?.mobile)
+            setNumber(values?.mobile)
+        sendData(values)
+    }
+    const onSub = (values) => {
+        sendCode(values)
+    }
+    const sendData = (values) => {
+        setLoading(true)
+        axios.put(`${BASE_URL}${EDIT_PROFILE}`, values)
+            .then(resp => {
+                setLoading(false)
+                if (resp.data.code === 200) {
+
+                    setShowNumber(false)
+                    message.success(" موبایل شما با موفقیت ویرایش شد")
+                    // console.log(resp.data.data.result.mobile)
+                    // setNumber()
+                }
+            })
+            .catch(err => {
+                setLoading(false)
+                console.error(err);
+                message.error("دوباره تلاش کنید")
+            })
+    }
+    const sendCode = (values) => {
+        setLoading(true)
+        axios.post(`${BASE_URL}${ACCOUNT_APPROVE}`, {...values, user_name: number})
+            .then(resp => {
+                setLoading(false)
+                if (resp.data.code === 200) {
+                    setShowNumber(true)
+                    message.success(" موبایل شما با موفقیت تایید شد")
+                    getProfile()
+
+                }else {
+                    message.error(resp.data.result)
+                }
+            })
+            .catch(err => {
+                setLoading(false)
+                message.error("دوباره تلاش کنید")
+            })
+    }
     return (
-        <>
-                       <div>
-                    <div className="row">
-                      <div className="col-md-6">
-                        <div className="input-group">
-                          <label className="default-lable">شماره همراه</label>
-                          <input
-                            type="tel"
-                            className="default-input"
-                            placeholder="شماره موبایل خود را وارد نمایید."
-                          />
+
+        <Spin spinning={loading}>
+            {showNumber ? <Form onFinish={onFinish}
+                                form={form}
+                                wrapperCol={{span: 24}}>
+                    <div>
+                        <div>
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="input-group">
+                                        <label className="default-lable">شماره همراه</label>
+                                        <Form.Item
+                                            className="w-100"
+                                            name="mobile"
+                                            rules={[
+                                                {
+                                                    required: true,
+                                                    message: "تکمیل این فیلد ضروری است",
+                                                },
+                                                {
+                                                    min: 11,
+                                                    message: "11 کارکتر مورد نیاز است",
+                                                }
+                                            ]}>
+                                            <Input className="default-input"
+                                                   type="tel"
+                                                   placeholder="شماره موبایل خود را وارد نمایید."/>
+                                        </Form.Item>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-6 button-group">
+                                    <Button className="btn-default" htmlType="submit">
+                                        ثبت
+                                    </Button>
+                                </div>
+                            </div>
                         </div>
-                      </div>
                     </div>
-                    <div className="row">
-                      <div className="col-md-6 button-group">
-                        <button type="button" className="btn-default">
-                          ثبت
-                        </button>
-                      </div>
-                    </div>
-                    <div className="row mrgt50">
-                      <p className="darkgray dir-rtl">
-                        همکار گرامی، در این قسمت، بعد از اینکه کاربر شماره تماس
-                        خود را وارد نمود، قسمت بالا حذف شده و قسمت پایین نمایش
-                        داده می شود. لطفا کلاس mrgt50 را در این قسمت حذف کنید.
-                      </p>
-                      <p className="darkgray">
-                        ما یک کد به +98 912 ***** 9 ارسال کردیم ، برای تأیید
-                        شماره تلفن خود ، کد را در زیر وارد کنید.
-                      </p>
-                      <div className="col-md-6">
-                        <div className="input-group ">
-                          <label className="default-lable">کد تایید</label>
-                          <input
-                            type="text"
-                            className="default-input"
-                            placeholder="کد تایید را اینجا وارد نمایید."
-                          />
+                </Form>
+                :
+                <Form onFinish={onSub}
+                      form={form}
+                      wrapperCol={{span: 24}}>
+                    <div>
+                        <div>
+                            <div className="row">
+                                <div className="row ">
+
+                                    <p className="darkgray">
+                                        ما یک کد به
+                                        <span className="px-2">{number}</span>
+
+                                        ارسال کردیم ، برای تأیید
+                                        شماره تلفن خود ، کد را در زیر وارد کنید.
+                                    </p>
+                                    <div className="col-md-6">
+                                        <div className="input-group ">
+                                            <label className="default-lable">کد تایید</label>
+                                            <Form.Item
+                                                className="w-100"
+                                                name="verify_code"
+                                                rules={[
+                                                    {
+                                                        required: true,
+                                                        message: "تکمیل این فیلد ضروری است",
+                                                    }
+                                                ]}>
+                                                <Input className="default-input"
+                                                       type="text"
+                                                       placeholder="کد تایید را اینجا وارد نمایید."/>
+                                            </Form.Item>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-6 button-group">
+                                    <Button  className="btn-default" htmlType="submit">
+                                        تایید
+                                    </Button>
+                                    <Button  className="btn-default" onClick={()=>setShowNumber(true)}>
+                                        ویرایش موبایل
+                                    </Button>
+                                </div>
+                            </div>
                         </div>
-                      </div>
                     </div>
-                    <div className="row">
-                      <div className="col-md-6 button-group">
-                        <button type="button" className="btn-default">
-                          تایید
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-        </>
+                </Form>
+            }
+        </Spin>
     )
 }
 
--- src/Views/PanleProfile/PanelProfile.jsx
+++ src/Views/PanleProfile/PanelProfile.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {
     faCheckCircle,
@@ -16,17 +16,35 @@ import EditPanelProfile from "./EditPanelProfile";
 import ChangePasswordPanelProfile from "./ChangePasswordPanelProfile";
 import EditPhoneNumberPanelProfile from "./EditPhoneNumberPanelProfile";
 import EditEmailPanelProfile from "./EditEmailPanelProfile";
-import {Tabs} from "antd";
+import {message, Tabs} from "antd";
+import axios from "../../utils/request";
+import {BASE_URL} from "../../utils";
+import {EDIT_PROFILE} from "../../utils/constant";
 
 function PanelProfile() {
     const {TabPane} = Tabs;
     const [activeKey, setActiveKey] = useState("1")
+    const [data, setData] = useState({})
 
     function callback(key) {
         console.log(key);
         setActiveKey(key)
     }
-
+    useEffect(()=>{
+        getData()
+    },[])
+    const getData = () => {
+        axios.get(`${BASE_URL}${EDIT_PROFILE}`)
+            .then(resp => {
+                if ((resp.data.code === 200) && resp.data?.data?.result) {
+                    setData(resp.data.data.result)
+                }
+            })
+            .catch(err => {
+                console.error(err);
+                message.error("صفحه را دوباره لود کنید")
+            })
+    }
     return (
         <div dir="rtl">
             <HeaderPanel/>
@@ -37,16 +55,16 @@ function PanelProfile() {
                         <div className="panel-container">
                             <Tabs activeKey={activeKey} onChange={callback} className="nav nav-tabs main-tab" unmountInactiveTabs={true}>
                                 <TabPane tab=" ویرایش پروفایل" key="1" className="nav-item" >
-                                    <EditPanelProfile/>
+                                    <EditPanelProfile data={data} getProfile={getData}/>
                                 </TabPane>
                                 <TabPane tab=" تغییر رمز عبور" key="2" className="nav-item ">
-                                    <ChangePasswordPanelProfile/>
+                                    <ChangePasswordPanelProfile data={data} getProfile={getData}/>
                                 </TabPane>
                                 <TabPane tab="ویرایش شماره همراه" key="3" className="nav-item ">
-                                    <EditPhoneNumberPanelProfile/>
+                                    <EditPhoneNumberPanelProfile data={data} getProfile={getData}/>
                                 </TabPane>
                                 <TabPane tab="ویرایش ایمیل" key="4" className="nav-item ">
-                                    <EditEmailPanelProfile/>
+                                    <EditEmailPanelProfile data={data} getProfile={getData}/>
                                 </TabPane>
                             </Tabs>
                         </div>
