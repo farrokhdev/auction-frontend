ff256e8a769460a2465321ceec745aa9e5be8a74
ehsan mashali <ehsanmashali222.78@gmail.com>
2021-07-18 12:55:43 +0430

    fixe confilict and chang Ui in panel profile

--- src/Views/PanleProfile/EditPanelProfile.jsx
+++ src/Views/PanleProfile/EditPanelProfile.jsx
@@ -3,11 +3,14 @@ import {Button, Form, Input, Row, Col, message, Spin} from "antd";
 import TextArea from "antd/es/input/TextArea";
 import axios from "../../utils/request";
 import {BASE_URL} from "../../utils";
-import {EDIT_PROFILE} from "../../utils/constant"
+import {EDIT_PROFILE} from "../../utils/constant";
+import {Link} from "react-router-dom";
+
 function EditPanelProfile(props) {
     const [form] = Form.useForm();
     const [loading,setLoading]=useState(false)
     const {data} = props;
+    const {setActiveKey} =props;
     const onFinish = (values) => {
         sendData(values)
     }
@@ -93,27 +96,67 @@ function EditPanelProfile(props) {
                         </div>
                     </div>
                     <div className="col-md-6">
-                        <div className="input-group">
+
+                        
+                    {/* <div class="input-group notapproved">
+                        <label class="default-lable">ایمیل</label>
+                        {data.mobile &&  data.mobile.length ?
+                        <>
+                        <input type="email" class="default-input" placeholder="ایمیل خود را وارد نمایید."
+                               value="Nima.heirdari@gmail.com"/>
+                         <span class="input-note">برای تایید ایمیل خود اینجا کلیک کنید.</span>
+                        <span class="notapproved input-state">تایید نشده</span>
+                        </>
+                        :
+                        
+                        <button onClick={()=> setActiveKey("3")} className="input-note text-muted" >
+                        برای تغییر و تایید شماره موبایل خود اینجا کلیک کنید.
+                        </button>
+}
+                    </div> */}
+
+
+                        <div className="input-group notapproved">
                             <label className="default-lable">شماره همراه</label>
-                            <Form.Item
-                                className="w-100"
-                                name="mobile">
-                                <Input className="default-input"
-                                       placeholder="شماره موبایل مورد نظر را وارد نمایید."
-                                       disabled/>
-                            </Form.Item>
+                            {data.mobile &&  data.mobile.length ?
+                                <>
+                                    <Form.Item
+                                        className="w-100"
+                                        name="mobile">
+                                        <Input className="default-input"
+                                            placeholder="شماره موبایل مورد نظر را وارد نمایید."
+                                            disabled/>
+
+                                    </Form.Item>
+                                    <span class="approved input-state">تایید شده</span>
+                                </>
+                            :
+                            <button onClick={()=> setActiveKey("3")} className="input-note text-muted" >
+                            برای تغییر و تایید شماره موبایل خود اینجا کلیک کنید.
+                            </button>
+                        }
                         </div>
                     </div>
                     <div className="col-md-6">
-                        <div className="input-group ">
+                        <div className="input-group notapproved">
                             <label className="default-lable">ایمیل</label>
-                            <Form.Item
-                                className="w-100"
-                                name="email">
-                                <Input className="default-input"
-                                       placeholder="ایمیل خود را وارد نمایید."
-                                       disabled/>
-                            </Form.Item>
+                            { data.email && data.email.length ?
+                                <>
+                                    <Form.Item
+                                        className="w-100"
+                                        name="email">
+                                        <Input className="default-input"
+                                            placeholder="ایمیل خود را وارد نمایید."
+                                            disabled/>
+                                    </Form.Item>
+                                    <span class="approved input-state">تایید شده</span>
+                                </>
+                            :
+                            <button onClick={()=> setActiveKey("4")} className="input-note text-muted" >
+                            برای تغییر و تایید ایمیل خود اینجا کلیک کنید.
+                            </button>
+                        }
+
 
                         </div>
                     </div>
