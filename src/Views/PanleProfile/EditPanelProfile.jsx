1bdd146a0f993f925410863cf4341ef03e366c14
samira rezaei <samirarezai1996@gmail.com>
2021-07-19 17:40:35 +0430

    change address in profilePanel

--- src/Views/PanleProfile/EditPanelProfile.jsx
+++ src/Views/PanleProfile/EditPanelProfile.jsx
@@ -8,34 +8,35 @@ import {Link} from "react-router-dom";
 
 function EditPanelProfile(props) {
     const [form] = Form.useForm();
-    const [loading,setLoading]=useState(false)
+    const [loading, setLoading] = useState(false)
     const {data} = props;
-    const {setActiveKey} =props;
+    const {setActiveKey} = props;
     const onFinish = (values) => {
         sendData(values)
     }
-    useEffect(()=>{
+    useEffect(() => {
         form.setFieldsValue(data)
-    },[data])
+    }, [data])
 
 
+    const sendData = (values) => {
+        setLoading(true)
+        axios.put(`${BASE_URL}${EDIT_PROFILE}`, {...values, "home_auction_location": {
+                "address": values?.address || ""
+            }})
+            .then(resp => {
+                setLoading(false)
+                if (resp.data.code === 200) {
 
-  const sendData = (values) => {
-      setLoading(true)
-    axios.put(`${BASE_URL}${EDIT_PROFILE}`,values)
-        .then(resp => {
-            setLoading(false)
-          if (resp.data.code === 200) {
-
-              message.success("پروفایل شما با موفقیت ویرایش شد")
-          }
-        })
-        .catch(err => {
-            setLoading(false)
-          console.error(err);
-            message.error("دوباره تلاش کنید")
-        })
-  }
+                    message.success("پروفایل شما با موفقیت ویرایش شد")
+                }
+            })
+            .catch(err => {
+                setLoading(false)
+                console.error(err);
+                message.error("دوباره تلاش کنید")
+            })
+    }
 
     // const getData = () => {
     //     setLoading(true)
@@ -54,51 +55,51 @@ function EditPanelProfile(props) {
     // }
     return (
         <Spin spinning={loading}>
-        <Form onFinish={onFinish}
-              form={form}
-              wrapperCol={{span: 24}}>
+            <Form onFinish={onFinish}
+                  form={form}
+                  wrapperCol={{span: 24}}>
 
-            <div className="">
-                <div className="row">
-                    <div className="col-md-6">
-                        <div className="input-group w-100">
-                            <label className="default-lable">نام</label>
-                            <Form.Item
-                                className="w-100"
-                                name="first_name"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: "تکمیل این فیلد ضروری است",
-                                    }
-                                ]}>
-                                <Input className="default-input"
-                                       placeholder="نام خود را وارد نمایید."/>
-                            </Form.Item>
+                <div className="">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="input-group w-100">
+                                <label className="default-lable">نام</label>
+                                <Form.Item
+                                    className="w-100"
+                                    name="first_name"
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: "تکمیل این فیلد ضروری است",
+                                        }
+                                    ]}>
+                                    <Input className="default-input"
+                                           placeholder="نام خود را وارد نمایید."/>
+                                </Form.Item>
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-md-6">
-                        <div className="input-group">
-                            <label className="default-lable">نام خانوادگی</label>
-                            <Form.Item
-                                className="w-100"
-                                name="last_name"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: "تکمیل این فیلد ضروری است",
-                                    }
-                                ]}>
-                                <Input className="default-input"
-                                       placeholder="نام خانوادگی خود را وارد نمایید."/>
-                            </Form.Item>
+                        <div className="col-md-6">
+                            <div className="input-group">
+                                <label className="default-lable">نام خانوادگی</label>
+                                <Form.Item
+                                    className="w-100"
+                                    name="last_name"
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: "تکمیل این فیلد ضروری است",
+                                        }
+                                    ]}>
+                                    <Input className="default-input"
+                                           placeholder="نام خانوادگی خود را وارد نمایید."/>
+                                </Form.Item>
 
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-md-6">
+                        <div className="col-md-6">
 
-                        
-                    {/* <div class="input-group notapproved">
+
+                            {/* <div class="input-group notapproved">
                         <label class="default-lable">ایمیل</label>
                         {data.mobile &&  data.mobile.length ?
                         <>
@@ -116,94 +117,112 @@ function EditPanelProfile(props) {
                     </div> */}
 
 
-                        <div className="input-group notapproved">
-                            <label className="default-lable">شماره همراه</label>
-                            {data.mobile &&  data.mobile.length ?
-                                <>
-                                    <Form.Item
-                                        className="w-100"
-                                        name="mobile">
-                                        <Input className="default-input"
-                                            placeholder="شماره موبایل مورد نظر را وارد نمایید."
-                                            disabled/>
+                            <div className="input-group notapproved">
+                                <label className="default-lable">شماره همراه</label>
+                                {data.mobile && data.mobile.length ?
+                                    <>
+                                        <Form.Item
+                                            className="w-100"
 
-                                    </Form.Item>
-                                    <span class="approved input-state">تایید شده</span>
-                                </>
-                            :
-                            <button onClick={()=> setActiveKey("3")} className="input-note text-muted" >
-                            برای تغییر و تایید شماره موبایل خود اینجا کلیک کنید.
-                            </button>
-                        }
+                                            name="mobile">
+                                            <Input className="default-input"
+                                                   placeholder="شماره موبایل مورد نظر را وارد نمایید."
+                                                   disabled/>
+
+                                        </Form.Item>
+                                        <span className="approved input-state">تایید شده</span>
+                                    </>
+                                    :
+                                    <button onClick={() => setActiveKey("3")} className="input-note text-muted">
+                                        برای تغییر و تایید شماره موبایل خود اینجا کلیک کنید.
+                                    </button>
+                                }
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-md-6">
-                        <div className="input-group notapproved">
-                            <label className="default-lable">ایمیل</label>
-                            { data.email && data.email.length ?
-                                <>
-                                    <Form.Item
-                                        className="w-100"
-                                        name="email">
-                                        <Input className="default-input"
-                                            placeholder="ایمیل خود را وارد نمایید."
-                                            disabled/>
-                                    </Form.Item>
-                                    <span class="approved input-state">تایید شده</span>
-                                </>
-                            :
-                            <button onClick={()=> setActiveKey("4")} className="input-note text-muted" >
-                            برای تغییر و تایید ایمیل خود اینجا کلیک کنید.
-                            </button>
-                        }
+                        <div className="col-md-6">
+                            <div className="input-group notapproved">
+                                <label className="default-lable">ایمیل</label>
+                                {data.email && data.email.length ?
+                                    <>
+                                        <Form.Item
+                                            className="w-100"
+                                            name="email">
+                                            <Input className="default-input"
+                                                   placeholder="ایمیل خود را وارد نمایید."
+                                                   disabled/>
+                                        </Form.Item>
+                                        <span className="approved input-state">تایید شده</span>
+                                    </>
+                                    :
+                                    <button onClick={() => setActiveKey("4")} className="input-note text-muted">
+                                        برای تغییر و تایید ایمیل خود اینجا کلیک کنید.
+                                    </button>
+                                }
 
 
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="input-group">
+                                <label className="default-lable">کدپستی</label>
+                                <Form.Item
+                                    className="w-100"
+                                    name="postal_code"
+                                    rules={[
+                                        {
+                                            pattern: /\b(?!(\d)\1{3})[13-9]{4}[1346-9][013-9]{5}\b/,
+                                            message: " کدپستی صحیح وارد کنید",
+                                        },
+                                    ]}
+                                >
+                                    <input type="number" className="default-input"
+                                           placeholder="کد پستی خود را وارد نمایید."/>
+                                </Form.Item>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="input-group">
+                                <label className="default-lable">کدملی</label>
+                                <Form.Item
+                                    className="w-100"
+                                    name="national_code"
+                                    rules={[
+                                        {
+                                            min: 10,
+                                            message: "حداقل 10 رقم وارد کنید ",
+                                        },
+                                        {
+                                            max: 11,
+                                            message: "حداکثر 11 رقم وارد کنید ",
+                                        },
+                                    ]}
+                                >
+                                    <Input className="default-input"
+                                           placeholder="کد ملی خود را وارد نمایید."/>
+                                </Form.Item>
+                            </div>
+                        </div>
+                        <div className="col-12">
+                            <div className="input-group">
+                                <label className="default-lable">آدرس</label>
+                                <Form.Item
+                                    className="w-100"
+                                    name="address">
+                                    <TextArea className="default-input"
+                                              placeholder="آدرس خود را وارد نمایید."/>
+                                </Form.Item>
+                            </div>
                         </div>
                     </div>
-                    <div className="col-md-6">
-                        <div className="input-group">
-                            <label className="default-lable">کدپستی</label>
-                            <Form.Item
-                                className="w-100"
-                                name="postal_code">
-                                <Input className="default-input"
-                                       placeholder="کد پستی خود را وارد نمایید."/>
-                            </Form.Item>
-                        </div>
-                    </div>
-                    <div className="col-md-6">
-                        <div className="input-group">
-                            <label className="default-lable">کدملی</label>
-                            <Form.Item
-                                className="w-100"
-                                name="national_code"
-                             >
-                                <Input className="default-input"
-                                       placeholder="کد ملی خود را وارد نمایید."/>
-                            </Form.Item>
-                        </div>
-                    </div>
-                    <div className="col-12">
-                        <div className="input-group">
-                            <label className="default-lable">آدرس</label>
-                            <Form.Item
-                                className="w-100"
-                                name="address">
-                                <TextArea className="default-input"
-                                          placeholder="آدرس خود را وارد نمایید."/>
-                            </Form.Item>
+                    <div className="row">
+                        <div className="col-12 button-group">
+                            <Button className="btn-default" htmlType="submit">
+                                ثبت
+                            </Button>
                         </div>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-12 button-group">
-                        <Button className="btn-default" htmlType="submit">
-                            ثبت
-                        </Button>
-                    </div>
-                </div>
-            </div>
-        </Form>
+            </Form>
         </Spin>
     )
 }
