05b041e0b86f3b0b54677c0b4b7b2aad20596268
vahid ramezanpoor <md.ramezanpoor@gmail.com>
2021-07-26 11:06:44 +0000

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,7 @@
 export const BASE_URL = "https://api.beta.smartauctionhouse.com/api";
 // export const BASE_URL = "http://192.168.0.38:8010/api";
 // export const BASE_URL = "http://192.168.0.140:8003/api";
-export const BASE_URL = "http://192.168.0.182:9000/api";
+// export const BASE_URL = "http://192.168.0.182:9000/api";
 // export const BASE_URL ="http://192.168.0.182:9000/api";
 
 export const COOKIE_EXPIRES = 1;
