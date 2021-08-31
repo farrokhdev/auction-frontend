c8c56241a266dc23ab4933e637297511ae58dce9
samira rezaei <samirarezai1996@gmail.com>
2021-08-31 12:44:39 +0430

    bids detail

--- src/Views/MyPurchases/MyPurchases.jsx
+++ src/Views/MyPurchases/MyPurchases.jsx
@@ -74,6 +74,7 @@ function MyPurchases() {
                               
                                     price={item?.bidding_details?.max_bid ? item?.bidding_details?.max_bid : ''}
                                     paymentMethod={item?.payment_method}
+                                    media={item?.media}
                                     />) : null}
 
                             </div>
