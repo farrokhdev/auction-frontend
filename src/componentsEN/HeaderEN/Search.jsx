1c54a6c1efe07653ceb4e455a9c8c73a40f70262
reza semyari <rezasemyari1991@gmail.com>
2021-11-17 12:36:06 +0330

    edit url of web socket in index.js and add add tabs search bar ui in after login en page

--- src/componentsEN/HeaderEN/Search.jsx
+++ src/componentsEN/HeaderEN/Search.jsx
@@ -1,94 +1,63 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import pic5 from "../../imgEN/pic5.jpg"
+import React , {useState} from 'react';
+import { Tabs } from 'antd';
+import AllTab from './AllTab';
+import AuctionsTab from './AuctionsTab';
+import ArtworksTab from './ArtworksTab';
+import AuctionHousesTab from './AuctionHousesTab';
+import RecentlySearch from './RecentlySearch';
+
 function Search() {
+
+    const { TabPane } = Tabs;
+    const [activeKey, setActiveKey] = useState("1")
+
+    function callback(key) {
+        setActiveKey(key)
+    }
+
+
     return (
         <>
-            <div className="inner-nav" id="nav-search" aria-labelledby="#navsearch">
+           <div className="inner-nav" id="nav-search" aria-labelledby="#navsearch">
                 <div className="container containercs">
                     <div className="row">
                         <div className="col-lg-6">
                             <div className="main-search">
-                                <input placeholder="Search artworks, auctions, auctions home ...." />
+                                <input placeholder="Search artworks, auctions, auctions home ...."/>
                                 <button type="button" className="btn btn-view">Advance</button>
                             </div>
                         </div>
                     </div>
                     <div className="row mrgt30">
+
+
+                        <RecentlySearch/>
+
+     
                         <div className="col-md-9 col-lg-10">
                             <div className="category-search">
-                                <ul className="nav nav-tabs justify-content-star main-tab" id="cat-serach"
-                                    role="tablist">
-                                    <li className="nav-item" role="presentation">
-                                        <button className="nav-link active" id="tab-11" data-bs-toggle="tab"
-                                            data-bs-target="#catsearch1"
-                                            type="button" role="tab" aria-controls="catsearch1"
-                                            aria-selected="true">All
-                                        </button>
-                                    </li>
-                                    <li className="nav-item" role="presentation">
-                                        <button className="nav-link" id="tab-21" data-bs-toggle="tab"
-                                            data-bs-target="#catsearch2"
-                                            type="button" role="tab" aria-controls="tab2"
-                                            aria-selected="false">Auctions
-                                        </button>
-                                    </li>
-                                    <li className="nav-item" role="presentation">
-                                        <button className="nav-link" id="tab-31" data-bs-toggle="tab"
-                                            data-bs-target="#catsearch3"
-                                            type="button" role="tab" aria-controls="tab3"
-                                            aria-selected="false">Artworks
-                                        </button>
-                                    </li>
-                                    <li className="nav-item" role="presentation">
-                                        <button className="nav-link" id="tab-41" data-bs-toggle="tab"
-                                            data-bs-target="#catsearch4"
-                                            type="button" role="tab" aria-controls="tab4"
-                                            aria-selected="false">Auction
-                                            homes
-                                        </button>
-                                    </li>
-                                </ul>
-                                <div className="tab-content main-tab-content" id="cat-serach-content">
-                                    <div className="tab-pane fade show active" id="catsearch1"
-                                        role="tabpanel"
-                                        aria-labelledby="home-tab">
-                                        <div className="owl-carousel">
-                                            <Link to="/" className="artwork-block">
-                                                <div className="artwork-img">
-                                                    <div className="bg-shadow tl-shadow10">
-                                                        <img src={pic5} width="50" height="80"
-                                                        style={{display:'block' , width:'50%' ,}}
-                                                            alt=""
-                                                            className="" />
-                                                        <div className="artwork-category">
-                                                            <span className="category-save artwork-bookmark"></span>
-                                                            <span className="category-icon firstoffer-icon">First offer</span>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div className="block-body">
-                                                    <h4 className="default">Windows from the Red series</h4>
-                                                    <h6 className="default">Parivash Ganji</h6>
-                                                    <div className="auction-calender">
-                                                        <div className="auction-date">
-                                                            <span className="start-date">19 June</span>
-                                                            <span className="end-date">22 June</span>
-                                                        </div>
-                                                        <div className="auction-time">
-                                                            <span className="start-time">10 AM</span>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </Link>
-                                        </div>
-                                    </div>
-                                </div>
+
+                                <Tabs activeKey={activeKey} onChange={callback}
+                                    className="nav nav-tabs justify-content-star main-tab " unmountInactiveTabs={true}>
+                                    <TabPane tab="All" key="1" className="nav-item ">
+                                        <AllTab/>
+                                    </TabPane>
+                                    <TabPane tab="Auctions" key="2" className="nav-item ">
+                                        <AuctionsTab/>
+                                    </TabPane>
+                                    <TabPane tab="Artworks" key="3" className="nav-item ">
+                                        <ArtworksTab/>
+                                    </TabPane>
+                                    <TabPane tab="Auction homes" key="4" className="nav-item ">
+                                        <AuctionHousesTab/>
+                                    </TabPane>
+                                </Tabs>
+
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div>                 
         </>
     )
 }
