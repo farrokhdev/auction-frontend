7ac78d09840b29e3dd68ff55406b4d842fafff28
reza semyari <rezasemyari1991@gmail.com>
2021-11-29 17:56:02 +0330

    add api call panel wallet user

--- src/viewsEN/PanelWallet/PanelWallet.jsx
+++ src/viewsEN/PanelWallet/PanelWallet.jsx
@@ -1,14 +1,35 @@
-import React , {useState} from "react";
+import React , {useState , useEffect} from "react";
 import HeaderPanel from "../../componentsEN/HeaderPanel";
 import PanelSideBar from "../../componentsEN/PanelSideBar/PanelSideBar";
 import ModalIncreaseCredit from "./ModalIncreaseCredit";
 import ModalWithdrawal from "./ModalWithdrawal";
-
+import axios from "../../utils/request";
+import {BASE_URL} from "../../utils";
 
 function PanelWallet() {
 
     const [visibleModalIncreaseCredit, setVisibleModalIncreaseCredit] = useState(false)
     const [visibleModalWithdrawal, setVisibleModalWithdrawal] = useState(false)
+    const [Wallet, setWallet] = useState("");
+    let numeral = require('numeral');
+
+    const getWallet = () => {
+        axios.get(`${BASE_URL}/accounting/wallet/me/`)
+            .then(resp => {
+                if (resp.data.code === 200) {
+                    setWallet(resp.data.data.result)
+                }
+
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    }
+
+    useEffect(() => {
+        getWallet()
+    }, [])
+
 
     return (
         <>
@@ -24,12 +45,12 @@ function PanelWallet() {
                             <div class="col">
                                 <div class="wallet-block">
                                     <h3 class="default gray50">Cash credit</h3>
-                                    <h2 class="default">35.000 USD</h2>
-                                    <div class="btn-group">
-                                        <button onClick={()=>setVisibleModalIncreaseCredit(true)} type="button" class="btn-default" data-bs-toggle="modal"
+                                    <h2 class="default">{numeral(Wallet?.inventory).format('0,0')} USD</h2>
+                                    <div class="btn-group d-block d-sm-flex justify-content-around">
+                                        <button onClick={()=>setVisibleModalIncreaseCredit(true)} type="button" class="btn-default m-0 mx-sm-3" data-bs-toggle="modal"
                                                 data-bs-target="#increasecreadit">Increase credit
                                         </button>
-                                        <button onClick={()=>setVisibleModalWithdrawal(true)} type="button" class="btn-outline-pink" data-bs-toggle="modal"
+                                        <button  onClick={()=>setVisibleModalWithdrawal(true)} type="button" class="btn-outline-pink mt-3 mt-sm-0" data-bs-toggle="modal"
                                                 data-bs-target="#withdrawal">Withdrawal
                                         </button>
                                     </div>
@@ -38,7 +59,7 @@ function PanelWallet() {
                             <div class="col">
                                 <div class="wallet-block">
                                     <h3 class="default gray50">Gift credit</h3>
-                                    <h2 class="default">0</h2>
+                                    <h2 class="default">{Wallet?.gift_credit}</h2>
                                 </div>
                             </div>
                         </div>
@@ -47,11 +68,15 @@ function PanelWallet() {
                 <ModalIncreaseCredit
                     setVisibleModalIncreaseCredit={setVisibleModalIncreaseCredit}
                     visibleModalIncreaseCredit={visibleModalIncreaseCredit}
+                     Wallet={Wallet}
+                    getWallet={getWallet}
                 />    
                 
                 <ModalWithdrawal
                     setVisibleModalWithdrawal={setVisibleModalWithdrawal}
                     visibleModalWithdrawal={visibleModalWithdrawal}
+                    Wallet={Wallet}
+                    getWallet={getWallet}
                 />
 
 
